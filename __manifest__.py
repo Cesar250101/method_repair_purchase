3b1d667f6264464282dc4eaa1554f63d4f9fbe20
Your Name <you@example.com>
2020-11-29 19:51:33 -0300

    Repair

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mrp'],
+    'depends': ['base','repair'],
 
     # always loaded
     'data': [
